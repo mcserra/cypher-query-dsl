ce5ae6d474fe959cec33314ff652299ca521b92e
Rita <ctw00158@criticaltechworks.com>
2020-04-27 10:01:23 +0100

    [#3] code review changes

--- src/main/java/com/dsl/expressions/bool/Contains.java
+++ src/main/java/com/dsl/expressions/bool/Contains.java
@@ -2,13 +2,13 @@ package com.dsl.expressions.bool;
 
 import com.dsl.expressions.AbstractExpression;
 import com.dsl.expressions.Expression;
-import com.dsl.expressions.ExpressionGalore;
+import com.dsl.expressions.param.Literal;
 import com.dsl.expressions.param.SelectorExpression;
 
 public class Contains extends AbstractExpression {
 
-    public Contains(final SelectorExpression e, final Expression... expressions) {
-        super(e, "CONTAINS", new ExpressionGalore(expressions));
+    public Contains(final SelectorExpression e, final Expression ex) {
+        super(e, "CONTAINS", ex);
     }
 
 }
